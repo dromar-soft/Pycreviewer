b47a4a296d850cf13f6d421bb1635a8743271e6d
dromar <mcbkair116@hotmail.co.jp>
2020-05-09 12:47:15 +0900

    Modify recieve request logic.

--- pycreviewer.py
+++ pycreviewer.py
@@ -5,6 +5,12 @@ from cparser import c_parser_wrapper,ast_analyser
 from ui import cui_view,presenter
 import time 
 
+
+def code_review_file():
+
+    time.sleep(0.5)
+    return "ALL OK."
+
 if __name__ == "__main__":
 
     print("pycreviewer start")
@@ -14,20 +20,33 @@ if __name__ == "__main__":
     myView.startup()
 
     #wait user input sourcefolder in ui thread
-    recvMsg = None
-    while recvMsg == None:
+    sourcefolder = None
+    while True:
         recvMsg = myPresenter.recieve_request(timeout=0.1)
-    print("id:"+recvMsg.id) 
-    print("data:"+recvMsg.data) 
+        if not recvMsg:
+            pass
+        elif recvMsg.id == "start_request":
+            sourcefolder = recvMsg.data
+            break
+        else:
+            pass
 
-    #for each file
-        #execute codereview
-        #output results in ui thread
-        #if user cancelled process from ui thread, then break for loop
-    #end
-    iscancelled = None
-    while iscancelled == None:
-        iscancelled = myPresenter.recieve_request(timeout=0.1)
-        myPresenter.send_review_results("results")
+    #create file list
+
+
+    # #for each file
+    #     #execute codereview
+    #     #output results in ui thread
+    #     #if user cancelled process from ui thread, then break for loop
+    # #end
+    while True:
+        recvMsg = myPresenter.recieve_request(timeout=0.1)
+        if not recvMsg:
+            results = code_review_file()
+            myPresenter.send_review_results(results)
+        elif recvMsg.id == "cancel_request":
+            break
+        else:
+            pass
 
     print("pycreviewer end")
