354f00ffa0899ec770c860e12fad8a516bbcded1
dromar <mcbkair116@hotmail.co.jp>
2020-05-14 19:03:27 +0900

    Add StaticVariablePrefix,GlobalVariablePrefix,VariableLengthMin Property in CheckConditions class.

--- review_rules/check_conditions.py
+++ review_rules/check_conditions.py
@@ -13,8 +13,36 @@ class CheckConditions():
     def __init__(self, file_path:str):
         self.__load__(file_path)
 
-    def Version(self):
-        if 'version' in self.json_dict:
-            return self.json_dict["version"]
+    def __StrValueInCondtions__(self, key:str)->str:
+        if key in self.__conditionsKey__():
+            return self.__conditionsKey__()[key]
         else:
             return ''
+
+    def __IntValueInCondtions__(self, key:str)->int:
+        if key in self.__conditionsKey__():
+            return self.__conditionsKey__()[key]
+        else:
+            return 0
+
+    def __conditionsKey__(self):
+        if 'conditions' in self.json_dict:
+            return self.json_dict['conditions']
+        else:
+            return []
+
+    def Version(self):
+        if 'version' in self.json_dict:
+            return self.json_dict['version']
+        else:
+            return ''
+
+    def StaticVariablePrefix(self)->str:
+        return self.__StrValueInCondtions__('static_variable_prefix')
+
+    def GlobalVariablePrefix(self)->str:
+        return self.__StrValueInCondtions__('global_variable_prefix')
+
+    def VariableLengthMin(self)->int:
+        return self.__IntValueInCondtions__('variable_length_min')
+        
--- review_rules/test_check_conditions.py
+++ review_rules/test_check_conditions.py
@@ -10,9 +10,45 @@ class TestCheckConditions(unittest.TestCase):
         obj = check_conditions.CheckConditions("./test_data/default.json")
         self.assertEqual(obj.Version(), "0.1.0")
 
-    def test_Version_noVersionKey(self):
-        obj = check_conditions.CheckConditions("./test_data/no_version_key.json")
+    def test_Version_NoVersionKey(self):
+        obj = check_conditions.CheckConditions("./test_data/no_all_key.json")
         self.assertEqual(obj.Version(), '')
+
+    def test_StaticVariablePrefix_valid(self):
+        obj = check_conditions.CheckConditions("./test_data/default.json")
+        self.assertEqual(obj.StaticVariablePrefix(), 'm_')
+    
+    def test_StaticVariablePrefix_NoAllKey(self):
+        obj = check_conditions.CheckConditions("./test_data/no_all_key.json")
+        self.assertEqual(obj.StaticVariablePrefix(), '')
+
+    def test_StaticVariablePrefix_NoConditionKey(self):
+        obj = check_conditions.CheckConditions("./test_data/no_conditions_key.json")
+        self.assertEqual(obj.StaticVariablePrefix(), '')
+
+    def test_GlobalVariablePrefix_valid(self):
+        obj = check_conditions.CheckConditions("./test_data/default.json")
+        self.assertEqual(obj.GlobalVariablePrefix(), 'g_')
+
+    def test_GlobalVariablePrefix_NoAllKey(self):
+        obj = check_conditions.CheckConditions("./test_data/no_all_key.json")
+        self.assertEqual(obj.GlobalVariablePrefix(), '')
+
+    def test_GlobalVariablePrefix_NoConditionKey(self):
+        obj = check_conditions.CheckConditions("./test_data/no_conditions_key.json")
+        self.assertEqual(obj.GlobalVariablePrefix(), '')
+
+    def test_VarialeLengthMin_valid(self):
+        obj = check_conditions.CheckConditions("./test_data/default.json")
+        self.assertEqual(obj.VariableLengthMin(), 2)
+
+    def test_VarialeLengthMin_NoAllKey(self):
+        obj = check_conditions.CheckConditions("./test_data/no_all_key.json")
+        self.assertEqual(obj.VariableLengthMin(), 0)
+
+    def test_VarialeLengthMin_NoConditionKey(self):
+        obj = check_conditions.CheckConditions("./test_data/no_conditions_key.json")
+        self.assertEqual(obj.VariableLengthMin(), 0)
 
 
 if __name__ == '__main__':
