1d4dc24e2ab03e7dd7b7f1023ac8245ba9693b85
dromar <mcbkair116@hotmail.co.jp>
2020-05-19 11:20:47 +0900

    Add tests module debug  launch because of vscode Python test function does not work suddenly.

---
+++ tests/__main__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+from .test_check_conditions import TestCheckConditions 
+from .test_source_code import TestSourceCode
+
+if __name__ == '__main__':
+    # スクリプトとして実行された場合の処理
+    print('unittest_main')
+    unittest.main()
