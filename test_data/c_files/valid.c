15c98b5acfd821819e4de804b28277edab31db8d
dromar <mcbkair116@hotmail.co.jp>
2020-05-20 10:23:27 +0900

    Improve test patterns of TestSourceCode.

--- test_data/c_files/valid.c
+++ test_data/c_files/valid.c
@@ -2,7 +2,7 @@ static int m_int_val1;
 int g_int_val1;
 static const long m_const_long_val2;
 const long g_const_long_val2;
-static volatile char m_volatile_char_val3;
+static volatile char volatile_char_val3; //static_variable_prefix NG
 volatile char g_volatile_char_val3;
 static int m_function_def2(char);
 void g_function_def1(char flag){
--- tests/test_source_code.py
+++ tests/test_source_code.py
@@ -5,7 +5,12 @@ from pycreviewer.source_code import SourceCode
 from pycreviewer.source_file_parser import parse
 
 class TestSourceCode(unittest.TestCase):
-    """SourceCodeクラステストを記述するクラス"""
+    """SourceCodeクラステストを記述するクラス
+            正常系のテスト観点：
+                期待する箇所で検索できていること->件数で判断->len(戻り値)
+                検索値が正しいこと->代表例１件で判定
+                coordについてはテストデータの管理が難しいため、ここではチェックしない。専用のテストケースを用意する
+    """
 
     def setUp(self):
 
@@ -20,10 +25,9 @@ class TestSourceCode(unittest.TestCase):
     def test_StaticValiables_valid(self):
         target = SourceCode(self.valid_ast)
         valiables = target.StaticValiables()
-        self.assertEqual(valiables[0].Name(), 'm_int_val1')
-        self.assertEqual(valiables[1].Name(), 'm_const_long_val2')
-        self.assertEqual(valiables[2].Name(),'m_volatile_char_val3')
         self.assertEqual(len(valiables), 3)
+        example = valiables[0]
+        self.assertEqual(example.Name(), 'm_int_val1')
     def test_StaticValiables_None(self):
         target = SourceCode(self.none_ast)
         valiables = target.StaticValiables()
@@ -32,10 +36,9 @@ class TestSourceCode(unittest.TestCase):
     def test_GlobalValiables_valid(self):
         target = SourceCode(self.valid_ast)
         valiables = target.GlobalValiables()
-        self.assertEqual(valiables[0].Name(), 'g_int_val1')
-        self.assertEqual(valiables[1].Name(), 'g_const_long_val2')
-        self.assertEqual(valiables[2].Name(), 'g_volatile_char_val3')
         self.assertEqual(len(valiables), 3)
+        example = valiables[0]
+        self.assertEqual(example.Name(), 'g_int_val1')
     def test_GlobalValiables_None(self):
         target = SourceCode(self.none_ast)
         valiables = target.GlobalValiables()
@@ -44,9 +47,9 @@ class TestSourceCode(unittest.TestCase):
     def test_DefinedFunctions_valid(self):
         target = SourceCode(self.valid_ast)
         functions = target.DefinedFunctions()
-        self.assertEqual(functions[0].Name(), 'g_function_def1')
-        self.assertEqual(functions[1].Name(), 'm_function_def2')
         self.assertEqual(len(functions), 2)
+        example = functions[0]
+        self.assertEqual(example.Name(), 'g_function_def1')
     def test_DefinedFunctions_None(self):
         target = SourceCode(self.none_ast)
         functions = target.DefinedFunctions()
@@ -54,14 +57,10 @@ class TestSourceCode(unittest.TestCase):
 
     def test_SearchReculsiveFunctionCall_valid(self):
         target = SourceCode(self.valid_ast)
-        funccall = target.SearchReculsiveFunctionCall()
-        self.assertEqual(funccall[0].Name(), 'g_function_def1')
-        self.assertEqual(funccall[1].Name(), 'g_function_def1')
-        self.assertEqual(funccall[2].Name(), 'g_function_def1')
-        self.assertEqual(funccall[3].Name(), 'g_function_def1')
-        self.assertEqual(funccall[4].Name(), 'g_function_def1')
-        self.assertEqual(funccall[5].Name(), 'g_function_def1')
-        self.assertEqual(len(funccall), 6)
+        funccalls = target.SearchReculsiveFunctionCall()
+        self.assertEqual(len(funccalls), 6)
+        example = funccalls[0]
+        self.assertEqual(example.Name(), 'g_function_def1')
     def test_SearchReculsiveFunctionCall_None(self):
         target = SourceCode(self.none_ast)
         steps = target.SearchReculsiveFunctionCall()
