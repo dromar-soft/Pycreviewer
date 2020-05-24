92a18ce6dede7f86a42e9819404f1c4d8caf0350
dromar <mcbkair116@hotmail.co.jp>
2020-05-24 10:06:24 +0900

    Rename execute_code_review -> review_file. Improve review_file() args. Add comment about review_file and __main__.

--- pycreviewer/__main__.py
+++ pycreviewer/__main__.py
@@ -5,8 +5,6 @@ from pathlib import Path
 import time 
 import os
 
-print(__file__)
-
 import pycreviewer
 from .source_code import SourceCode
 from .source_file_parser import parse
@@ -16,10 +14,17 @@ from .view_model_communication import ViewModelCommunicaton
 from .coding_rules import CheckResult, CodinfgRules
 
 
-def execute_code_review(source_path: str) ->list:
+def review_file(filepath: str, cpp_args=['-E', r'-Ipycreviewer/utils/fake_libc_include']) ->list:
+    """
+    単一のソースファイルに対してコードレビューを実施する。
+    コードレビューの結果は、List<CheckResult>形式で返す。
+    'filepath'には対象のソースファイルパスを入力する
+    'cppargs'にはCコンパイラのプリプロセッサ実行時のコマンドライン引数をリスト形式で入力する。
+    通常、'cppargs'には、プリプロセッサ実行オプション'-E'と、インクルードオプション'-Ixxxxx'を指定する
+    """
 
-    print("Executing: "+source_path)
-    ast = parse(filepath=source_path,cpp_args=['-E', r'-Ipycreviewer/utils/fake_libc_include'])
+    print("Executing: "+filepath)
+    ast = parse(filepath=filepath,cpp_args=cpp_args)
     code = SourceCode(ast)
     rules = CodinfgRules(code, CheckConditions('./default.json'))
     results = rules.check_all()
@@ -31,8 +36,14 @@ def search_csource_abs_paths(source_folder: str) ->list:
     return [str(path.absolute()) for path in source_path_list]
 
 if __name__ == "__main__":
+    """
+    pycreviewerライブラリを使用した簡単なCUIアプリケーション機能を提供する
+    本アプリケーションは、コンソール上でレビュー対象のソースフォルダパスの入力を受け付ける。
+    フォルダパスの入力受付後、フォルダパス内にある'.c'ファイルを再起的に検索し、各ファイルについてreview_file()を実行する。
+    ユーザは'esc'キー入力によって実行中のコードレビューを中止することができる
+    """
 
-    print("pycreviewer start")
+    print("pycreviewer cui application start")
 
     communication = ViewModelCommunicaton()
     view = CuiView(communication)
@@ -60,12 +71,13 @@ if __name__ == "__main__":
     # #end
     for source_file_path in source_file_paths:
         recvMsg = communication.recieve_request_from_view(timeout=0.1)
-        if not recvMsg:
-            results = execute_code_review(source_file_path)
+        if recvMsg is None:
+            #コードレビュー処理を継続
+            results = review_file(source_file_path)
             communication.send_review_results(results)
         elif recvMsg.id == "cancel_request":
             break
         else:
             pass
 
-    print("pycreviewer end")
+    print("pycreviewer cui application end")
